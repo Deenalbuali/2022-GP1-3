91a53ebd91076e164fdc7c146122d635d40b4334
rahaf-alzahrani <alzahranirahaf01@gmail.com>
2022-10-08 21:38:44 +0300

    2

--- elfaa/lib/main.dart
+++ elfaa/lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:elfaa/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-Future main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
