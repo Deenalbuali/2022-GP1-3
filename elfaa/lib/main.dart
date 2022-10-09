7ad768a9777b963c210bb009adf2a4fe3c8f376c
rahaf-alzahrani <alzahranirahaf01@gmail.com>
2022-10-09 13:02:45 +0300

    try

--- elfaa/lib/main.dart
+++ elfaa/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
+Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
