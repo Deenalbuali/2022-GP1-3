735044884899b021d8c4f46bc404695184435abc
Layanor <98930504+Layanor@users.noreply.github.com>
2022-10-09 13:21:23 +0300

    Update main.dart

--- elfaa/lib/main.dart
+++ elfaa/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-Future main() async{
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -25,6 +26,25 @@ class MyApp extends StatelessWidget {
               .collection('testing')
               .add({'timestamp': Timestamp.fromDate(DateTime.now())}),
           child: Icon(Icons.add),
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('testing').snapshots(),
+          builder: (
+            BuildContext context,
+            AsyncSnapshot<QuerySnapshot> snapshot,
+          ) {
+            if (!snapshot.hasData) return const SizedBox.shrink();
+            return ListView.builder(
+              itemCount: snapshot.data?.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                final docData = snapshot.data?.docs[index].data();
+                final dateTime = (docData!['timestamp'] as Timestamp).toDate();
+                return ListTile(
+                  title: Text(dateTime.toString()),
+                );
+              },
+            );
+          },
         ),
       ),
     );
