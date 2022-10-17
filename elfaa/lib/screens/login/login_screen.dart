b1739af4efdef489cfa5a95d65486b1e683f76b3
DanaSami20011 <98522245+DanaSami20011@users.noreply.github.com>
2022-10-17 16:37:08 +0300

    added my content front-end

--- elfaa/lib/screens/login/login_screen.dart
+++ elfaa/lib/screens/login/login_screen.dart
@@ -1,14 +1,118 @@
 import 'package:flutter/material.dart';
-import '../../components/background.dart';
+import 'package:elfaa/screens/signup/signup_screen.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
 
   @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController email = TextEditingController();
+  TextEditingController pass = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  @override
   Widget build(BuildContext context) {
-    return Background (
-      child: SingleChildScrollView(
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFF7EE),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(
+                  20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+              child: Column(
+                children: <Widget>[
+                  logoWidget("assets/img/logo1.png"),
+                  const SizedBox(height: 80),
+                  Directionality(
+                      textDirection: TextDirection.rtl,
+                      child: TextFormField(
+                        textAlign: TextAlign.right,
+                        controller: email,
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(Icons.email_outlined,
+                              color: Color(0xFFFD8601)),
+                          labelText: "البريد الإلكتروني",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || email.text.trim() == "") {
+                            return "الحقل مطلوب";
+                          }
+                        },
+                      )),
+                  const SizedBox(height: 20),
+                  Directionality(
+                      textDirection: TextDirection.rtl,
+                      child: TextFormField(
+                        obscureText: true,
+                        controller: pass,
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(Icons.lock_outline,
+                              color: Color(0xFFFD8601)),
+                          labelText: "كلمة السر",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty && pass.text.trim() != "") {
+                            return "الحقل مطلوب";
+                          }
+                        },
+                      )),
+                  const SizedBox(height: 50),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        //if all fields are valid
+                        //add to DB
+                      }
+                    },
+                    child: Text("تسجيل الدخول",
+                        style: TextStyle(color: Colors.white)),
+                  ),
+                  const SizedBox(height: 10),
+                  signUpOption()
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget logoWidget(String imageName) {
+    return Center(
+        child: Image.asset(
+      imageName,
+      fit: BoxFit.fill,
+      width: 150,
+      height: 150,
+    ));
+  }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SignupPage()));
+          },
+          child: const Text(
+            "تسجيل جديد ",
+            style: TextStyle(
+                fontSize: 17,
+                color: Color.fromARGB(255, 129, 129, 129),
+                fontWeight: FontWeight.w800),
+          ),
+        ),
+        const Text("ليس لديك حساب؟",
+            style: TextStyle(
+                fontSize: 17, color: Color.fromARGB(255, 129, 129, 129))),
+      ],
     );
   }
 }
--- elfaa/lib/screens/signup/signup_screen.dart
+++ elfaa/lib/screens/signup/signup_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../components/background.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -10,38 +9,103 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  TextEditingController email = TextEditingController();
+  TextEditingController pass = TextEditingController();
+  TextEditingController phoneNo = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    double w = MediaQuery.of(context).size.width;
-    double h = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Color(0xFFFFF7EE),
-      body: Column(
-        children: [
-          Container(
-            width: w,
-            height: h * 0.3,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/signupimg.png"),
-                    fit: BoxFit.cover)),
-          ), //container1
-          Container(
-            child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  "اهلًا بك",
-                  style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "!سجّل كمستخدم جديد في إلفاء",
-                  style: TextStyle(fontSize: 20, color: Colors.grey[500]),
-                )
-              ],
+      backgroundColor: const Color(0xFFFFF7EE),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          "تسجيل جديد",
+          style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFFFD8601)),
+        ),
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(
+                  20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+              child: Column(
+                children: <Widget>[
+                  Directionality(
+                      textDirection: TextDirection.rtl,
+                      child: TextFormField(
+                        textAlign: TextAlign.right,
+                        controller: email,
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(Icons.email_outlined,
+                              color: Color(0xFFFD8601)),
+                          labelText: "البريد الإلكتروني",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || email.text.trim() == "") {
+                            return "الحقل مطلوب";
+                          }
+                        },
+                      )),
+                  const SizedBox(height: 20),
+                  Directionality(
+                      textDirection: TextDirection.rtl,
+                      child: TextFormField(
+                        textAlign: TextAlign.right,
+                        controller: phoneNo,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(Icons.phone_callback,
+                              color: Color(0xFFFD8601)),
+                          labelText: "رقم الجوال",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || email.text.trim() == "") {
+                            return "الحقل مطلوب";
+                          }
+                        },
+                      )),
+                  const SizedBox(height: 20),
+                  Directionality(
+                      textDirection: TextDirection.rtl,
+                      child: TextFormField(
+                        obscureText: true,
+                        controller: pass,
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(Icons.lock_outline,
+                              color: Color(0xFFFD8601)),
+                          labelText: "كلمة السر",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty && pass.text.trim() != "") {
+                            return "الحقل مطلوب";
+                          }
+                        },
+                      )),
+                  const SizedBox(height: 50),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        //if all fields are valid
+                        //add to DB
+                      }
+                    },
+                    child: Text("تسجيل", style: TextStyle(color: Colors.white)),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
