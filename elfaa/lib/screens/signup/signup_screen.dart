a6858b118ec4f0c137734ff623f3d57d090bf9b6
DanaSami20011 <98522245+DanaSami20011@users.noreply.github.com>
2022-10-25 17:45:50 +0300

    Update signup_screen.dart

--- elfaa/lib/screens/signup/signup_screen.dart
+++ elfaa/lib/screens/signup/signup_screen.dart
@@ -135,7 +135,7 @@ class _SignupPageState extends State<SignupPage> {
                           if (value!.isEmpty || pass.text.trim() == "") {
                             return "الحقل مطلوب";
                           } else if (!regex.hasMatch(value)) {
-                            return "كلمة السر يجب أن تحتوي على حرف كبير، حرف صغير، ورقم";
+                            return "كلمة السر يجب أن تحتوي على حرف كبير باللغة الانجليزية، حرف صغير، ورقم";
                           } else if (value.length < 8) {
                             return "ادخل كلمة سر مكوّنة من 8 خانات على الأقل";
                           }
