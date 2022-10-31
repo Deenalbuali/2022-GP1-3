1d5ad4d5ab533804e526af22ed06f3dd51c928dd
DanaSami20011 <98522245+DanaSami20011@users.noreply.github.com>
2022-10-31 23:48:49 +0300

    First step successfully added

--- elfaa/lib/screens/login/ForgotPasswordPage.dart
+++ elfaa/lib/screens/login/ForgotPasswordPage.dart
@@ -12,6 +12,84 @@ class ForgotPasswordPage extends StatefulWidget {
 }
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  List<Step> getSteps() => [
+        Step(
+            isActive: currentStep >= 0,
+            title: Text('الخطوة 1'),
+            content: Container(
+                child: SingleChildScrollView(
+                    child: Form(
+                        key: _formKey,
+                        child: Padding(
+                            padding: EdgeInsets.fromLTRB(
+                                20,
+                                MediaQuery.of(context).size.height * 0.01,
+                                20,
+                                0),
+                            child: Column(
+                              children: <Widget>[
+                                logoWidget("assets/images/forgot.png"),
+                                Text("نسيت كلمة المرور؟",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: kPrimaryColor,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 25)),
+                                Text(
+                                    "الرجاء إدخال البريد الإلكتروني المرتبط بحسابك",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 129, 129, 129),
+                                        fontSize: 17)),
+                                const SizedBox(height: 50),
+                                Directionality(
+                                    textDirection: TextDirection.rtl,
+                                    child: TextFormField(
+                                      textAlign: TextAlign.right,
+                                      controller: email,
+                                      decoration: const InputDecoration(
+                                        suffixIcon: Icon(Icons.email_outlined,
+                                            color: Color(0xFFFD8601)),
+                                        labelText: "البريد الإلكتروني",
+                                        hintText: "أدخل بريدك الإلكتروني",
+                                      ),
+                                      validator: (value) {
+                                        if (value!.isEmpty ||
+                                            email.text.trim() == "") {
+                                          return "الحقل مطلوب";
+                                        } else if (!RegExp(
+                                                r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                                            .hasMatch(value)) {
+                                          return 'أدخل بريد إلكتروني صالح';
+                                        }
+                                      },
+                                    )),
+                                const SizedBox(height: 40),
+                                ElevatedButton(
+                                  onPressed: () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      // resetPassword();
+                                      sendOTP();
+                                    }
+                                  },
+                                  child: Text(" إرسال رمز التحقق",
+                                      style: TextStyle(color: Colors.white)),
+                                ),
+                              ],
+                            )))))),
+        Step(
+          isActive: currentStep >= 1,
+          title: Text("الخطوة 2"),
+          content: Container(),
+        ),
+        Step(
+          isActive: currentStep >= 2,
+          title: Text("اعادة تعيين كلمة المرور"),
+          content: Container(),
+        ),
+      ];
+
   void sendOTP() async {
     var emailauth = EmailAuth(sessionName: "إلفاء");
     if (await emailauth.sendOtp(recipientMail: email.text)) {
@@ -37,6 +115,8 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
 
   final _formKey = GlobalKey<FormState>();
   TextEditingController email = TextEditingController();
+  int currentStep = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,7 +136,22 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
           ),
           centerTitle: true,
         ),
-        body: Container(
+        body: Stepper(
+          type: StepperType.horizontal,
+          steps: getSteps(),
+          currentStep: currentStep,
+          onStepContinue: () {
+            final isLastStep = currentStep == getSteps().length - 1;
+            if (isLastStep) {
+              print("Completed");
+            }
+            setState(() => currentStep += 1);
+          },
+          onStepCancel:
+              currentStep == 0 ? null : () => setState(() => currentStep -= 1),
+        )
+
+        /*   Container(
             child: SingleChildScrollView(
                 child: Form(
                     key: _formKey,
@@ -113,7 +208,8 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
                                   style: TextStyle(color: Colors.white)),
                             ),
                           ],
-                        ))))));
+                        )))))*/
+        );
   }
 
   /* Future resetPassword() async {
