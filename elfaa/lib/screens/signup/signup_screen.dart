d82fd1d090c0aaa09c784922378bba3a38685c0b
DanaSami20011 <98522245+DanaSami20011@users.noreply.github.com>
2022-10-31 22:41:42 +0300

    Revert "هبد"
    This reverts commit 29f36e1745ca780e2df9f9e75bc0bb85f4f9591e.

--- elfaa/lib/screens/signup/signup_screen.dart
+++ elfaa/lib/screens/signup/signup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:elfaa/constants.dart';
 import 'package:elfaa/screens/login/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
