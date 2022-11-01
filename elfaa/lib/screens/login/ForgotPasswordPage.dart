1e56d7c9962fac42a7fe61a5bf61e64a9abb368e
DanaSami20011 <98522245+DanaSami20011@users.noreply.github.com>
2022-11-01 13:51:54 +0300

    update

--- elfaa/lib/screens/login/ForgotPasswordPage.dart
+++ elfaa/lib/screens/login/ForgotPasswordPage.dart
@@ -214,80 +214,25 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
           ),
           centerTitle: true,
         ),
-        body: Stepper(
-          type: StepperType.horizontal,
-          steps: getSteps(),
-          currentStep: currentStep,
-          onStepContinue: () {
-            final isLastStep = currentStep == getSteps().length - 1;
-            if (isLastStep) {
-              print("Completed");
-            }
-            setState(() => currentStep += 1);
-          },
-          onStepCancel:
-              currentStep == 0 ? null : () => setState(() => currentStep -= 1),
-        )
-
-        /*   Container(
-            child: SingleChildScrollView(
-                child: Form(
-                    key: _formKey,
-                    child: Padding(
-                        padding: EdgeInsets.fromLTRB(20,
-                            MediaQuery.of(context).size.height * 0.1, 20, 0),
-                        child: Column(
-                          children: <Widget>[
-                            logoWidget("assets/images/forgot.png"),
-                            Text("نسيت كلمة المرور؟",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: kPrimaryColor,
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 25)),
-                            Text(
-                                "الرجاء إدخال البريد الإلكتروني المرتبط بحسابك",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 129, 129, 129),
-                                    fontSize: 17)),
-                            const SizedBox(height: 50),
-                            Directionality(
-                                textDirection: TextDirection.rtl,
-                                child: TextFormField(
-                                  textAlign: TextAlign.right,
-                                  controller: email,
-                                  decoration: const InputDecoration(
-                                    suffixIcon: Icon(Icons.email_outlined,
-                                        color: Color(0xFFFD8601)),
-                                    labelText: "البريد الإلكتروني",
-                                    hintText: "أدخل بريدك الإلكتروني",
-                                  ),
-                                  validator: (value) {
-                                    if (value!.isEmpty ||
-                                        email.text.trim() == "") {
-                                      return "الحقل مطلوب";
-                                    } else if (!RegExp(
-                                            r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-                                        .hasMatch(value)) {
-                                      return 'أدخل بريد إلكتروني صالح';
-                                    }
-                                  },
-                                )),
-                            const SizedBox(height: 40),
-                            ElevatedButton(
-                              onPressed: () async {
-                                if (_formKey.currentState!.validate()) {
-                                  // resetPassword();
-                                  sendOTP();
-                                }
-                              },
-                              child: Text("إرسال",
-                                  style: TextStyle(color: Colors.white)),
-                            ),
-                          ],
-                        )))))*/
-        );
+        body: Theme(
+            data: Theme.of(context).copyWith(
+                colorScheme: ColorScheme.light(primary: kOrangeColor)),
+            child: Stepper(
+              type: StepperType.horizontal,
+              steps: getSteps(),
+              currentStep: currentStep,
+              onStepContinue: () {
+                final isLastStep = currentStep == getSteps().length - 1;
+                if (isLastStep) {
+                  print("Completed");
+                }
+                setState(() => currentStep += 1);
+              },
+              onStepCancel: currentStep == 0
+                  ? null
+                  : () => setState(() => currentStep -= 1),
+              //      controlsBuilder: (context , {onStepContinue,onStepCancel}),
+            )));
   }
 
   /* Future resetPassword() async {
