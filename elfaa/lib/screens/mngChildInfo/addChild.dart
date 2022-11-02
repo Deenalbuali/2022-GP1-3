8d233eea040f91138e5c5aebd1b9cafc38316d68
Renad Altayyar <reryxry@gmail.com>
2022-11-02 18:43:04 +0300

    Update addChild.dart

--- elfaa/lib/screens/mngChildInfo/addChild.dart
+++ elfaa/lib/screens/mngChildInfo/addChild.dart
@@ -116,10 +116,19 @@ class _addChildState extends State<addChild> {
                         return null;
                       },
                     )),
-                SizedBox(height: ScreenHeight * 0.025),
-                Directionality(
-                    textDirection: TextDirection.rtl,
-                    child: TextFormField(
+                SizedBox(height: ScreenHeight * 0.025),Directionality(
+                      textDirection: TextDirection.rtl,
+                    child: DropdownButtonFormField(
+                      decoration: const InputDecoration(
+                            suffixIcon: Icon(Icons.escalator_warning,
+                                color: Color(0xFFFD8601)),
+                            labelText: "الجنس",
+                          ),
+                      onChanged: (val){
+                        setState(() {
+                          selectedGender = val.toString();
+                        });
+                      },
                       value: selectedGender,
                       items: const [
                         DropdownMenuItem(child: Text("ذكر"), value: 'ذكر',),
