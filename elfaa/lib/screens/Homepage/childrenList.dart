87c9b62054e06fb4dbed1ada5c1948496d6ba576
rahaf-alzahrani <alzahranirahaf01@gmail.com>
2022-11-03 13:25:38 +0300

    Update childrenList.dart

--- elfaa/lib/screens/Homepage/childrenList.dart
+++ elfaa/lib/screens/Homepage/childrenList.dart
@@ -2,14 +2,19 @@ class childrenList {
   String? childImagePath;
   String? childName;
   String? childID;
-  int? zoneName;
+  String? childbirthday;
+  int? childHeight;
+  String? childGender;
   childrenList();
   Map<String, dynamic> toJson() =>
-      {'height': zoneName, 'image': childImagePath, 'name': childName};
+      {'image': childImagePath, 'name': childName, 'birthday': childbirthday, 'height':childHeight, 'gender': childGender};
 
   childrenList.fromSnapshot(snapshot)
-      : zoneName = snapshot.data()['height'],
+      : childHeight = snapshot.data()['height'],
         childName = snapshot.data()['name'],
         childID = snapshot.id,
-        childImagePath = snapshot.data()['image'];
+        childImagePath = snapshot.data()['image'],
+        childbirthday = snapshot.data()['birthday'],
+        childGender = snapshot.data()['gender'];
+
 }
