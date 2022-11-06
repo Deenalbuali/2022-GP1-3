247a2064392fc4e2c4e17bb8bf73e24952bed868
DanaSami20011 <98522245+DanaSami20011@users.noreply.github.com>
2022-11-06 15:27:00 +0300

    Update profile_page.dart

--- elfaa/lib/screens/profile/profile_page.dart
+++ elfaa/lib/screens/profile/profile_page.dart
@@ -220,7 +220,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
                       ),
                     ),
                     label: Text(
-                      '  تعديل بياناتي   ',
+                      ' حفظ التغييرات   ',
                       style: TextStyle(color: kPrimaryColor, fontSize: 20),
                     ),
                     onPressed: () async {
@@ -241,7 +241,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
                         final User? user = await _auth.currentUser;
                         if (!mounted) return;
                         final uid = user!.uid;
-                        try {
+                        try {  
                           await FirebaseFirestore.instance
                               .collection('users')
                               .doc(uid)
